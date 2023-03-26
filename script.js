da7af75b5d28bec11574e8a85f40d92146226f4f
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2023-03-26 16:20:54 -0300

    mexendo com tabelas

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+const form = document.getElementById("form")
+let ValidarForm= false
+let linha = '<tr>';
+
+
+/* Validar formulario*/
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+
+  linhasTabela()
+  AdicionarDadosTabela()
+
+  
+})
+/* criar as linhas da tabela */
+function linhasTabela(){
+  const inputNome= document.getElementById("nome")
+  const inputNumero= document.getElementById("numero")
+  const inputEmail= document.getElementById("email")
+
+  
+  linha += `<td> ${inputNome.value} </td>`
+  linha +=`<td> ${inputNumero.value} </td>`
+  linha +=`<td> ${inputEmail.value} </td>`
+  linha += '</tr>';
+}
+/* Adicionar dados na tabela */
+
+function AdicionarDadosTabela() {
+  const corpoTabela= document.querySelector("tbody")
+  corpoTabela.innerHTML=linha;
+}
+
+
+
 /*const taskInput = document.getElementById('task-input');
 const addBtn = document.getElementById('add-btn');
 const taskList = document.getElementById('task-list');
