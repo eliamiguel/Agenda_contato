17702c26599a0195b494521b6b934aab2528cf42
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2023-03-26 21:59:52 -0300

    sdss

--- script.js
+++ script.js
@@ -1,41 +1,70 @@
-const form = document.getElementById("form")
+const form = document.getElementById("form");
+const inputNome= document.getElementById("nome");
+  const inputNumero= document.getElementById("numero");
+  const inputEmail= document.getElementById("email");
+const totalContato= document.getElementById("QuantidadeNome")
+
 let ValidarForm= false
+let nome= [];
+let numero= [];
+let email= [];
 let linha = '<tr>';
 let linhas= " "; 
-
-
 
 
 /* Validar formulario*/
 form.addEventListener('submit', function(e){
   e.preventDefault();
 
-  linhasTabela()
-  AdicionarDadosTabela()
+linhasTabela();
+AdicionarDadosTabela();
+QuantidadeContatoAdicionado();
+  
+  
+
 
   
 })
 /* criar as linhas da tabela */
 function linhasTabela(){
-  const inputNome= document.getElementById("nome")
-  const inputNumero= document.getElementById("numero")
-  const inputEmail= document.getElementById("email")
-
+  if (nome.includes(inputNome.value)){
+    alert('Contato já adicionado!')
+  } else {
   
-  linha += `<td> ${inputNome.value} </td>`
-  linha +=`<td> ${inputNumero.value} </td>`
-  linha +=`<td> ${inputEmail.value} </td>`
-  linha += '</tr>';
-  linhas += linha;
+    nome.push(inputNome.value)
+    numero.push(parseFloat(inputNumero.value))
+    email.push(inputEmail.value)
+
+    linha += `<td> ${inputNome.value} </td>`
+    linha +=`<td> ${inputNumero.value} </td>`
+    linha +=`<td> ${inputEmail.value} </td>`
+    linha += '</tr>';
+    linhas += linha;
+  }
+  inputNome.value="";
+  inputNumero.value=" ";
+  inputEmail.value=" ";
 }
 /* Adicionar dados na tabela */
 
 function AdicionarDadosTabela() {
   linhas=linha;
-  const corpoTabela= document.querySelector("tbody")
+  const corpoTabela= document.querySelector("tbody");
   corpoTabela.innerHTML=linhas;
+
+  
 }
 
+function QuantidadeContatoAdicionado(){
+let somaNumero= 0
+  for ( c=0; c < numero.length; c ++){
+    somaNumero+= numero [c]
+   
+  }
+  totalContato.innerHTML= (c)
+  
+  
+}
 
 
 /*const taskInput = document.getElementById('task-input');
