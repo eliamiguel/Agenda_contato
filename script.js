da55ab7f9f5e8d140ee2636c0bfcd459f85b4c36
Elias Perreira Miguel <eliasperreiramiguel@gmail.com>
2023-03-26 22:53:02 -0300

    organizando

--- script.js
+++ script.js
@@ -54,38 +54,13 @@ function AdicionarDadosTabela() {
 
   
 }
+/* Total de contato adicionado */
 
 function QuantidadeContatoAdicionado(){
 let somaNumero= 0
   for ( c=0; c < numero.length; c ++){
     somaNumero+= numero [c]
-   
   }
   totalContato.innerHTML= (c)
-  
-  
 }
 
-
-/*const taskInput = document.getElementById('task-input');
-const addBtn = document.getElementById('add-btn');
-const taskList = document.getElementById('task-list');
-
-addBtn.addEventListener('click', () => {
-  if (taskInput.value !== '') {
-    const task = document.createElement('li');
-    task.classList.add('task');
-    task.innerHTML = `
-      <p>${taskInput.value}</p>
-      <button class="delete-btn">Excluir</button>
-    `;
-    taskList.appendChild(task);
-    taskInput.value = '';
-  }
-});
-
-taskList.addEventListener('click', (event) => {
-  if (event.target.classList.contains('delete-btn')) {
-    event.target.parentElement.remove();
-  }
-});*/
